9a17defea5ac7a76acc642c34b29ebb802f11d39
Matthew Hersh <mattmhersh@gmail.com>
2014-04-22 06:58:10 -0400

    Updated files

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -12,7 +12,10 @@ function IndexCtrl($scope, $http) {
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('IndexCtrl', [function() {
+  .controller('IndexCtrl', ['$scope', function($scope) {
+    
+    $scope.greeting = 'Hola!';
+    
     $scope.agent = "Matt Hersh";
   
     $scope.customer = { name: "Bob", address: "104 welsh dr." };
