4d6713e55c5194569e4a0c37d39394f8ae63eb95
Matthew Hersh <mattmhersh@gmail.com>
2014-04-22 16:31:53 -0400

    Added Directive

--- public/scripts/directives.js
+++ public/scripts/directives.js
@@ -2,14 +2,20 @@
 
 /* Directives */
 angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]).
-  directive('myCustomer', function() {
-    return {
-        restrict: 'E',
-        template: 'Customer Name: {{customer.name}} Customer Address: {{customer.address}}'
-    }
-});
+    directive('appVersion', ['version', function(version) {
+        return function(scope, elm, attrs) {
+          elm.text(version);
+        };
+    }])
+    .directive('myCustomer', function() {
+        return {
+            restrict: 'E',
+            template: 'Customer Name: {{customer.name}} Customer Address: {{customer.address}}'
+        }
+    })
+    .directive('mySample', function() {
+        return {
+            restrict: 'EC',
+            template: "<input type='text' ng-model='sampleData' /> {{sampleData}} <br />",
+        }
+    });
